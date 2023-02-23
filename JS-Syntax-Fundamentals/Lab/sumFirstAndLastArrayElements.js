7e4c47bc92259b0423e7f4a3f40292e35df8b2b0
mialskywalker <mialskywalker@gmail.com>
2023-02-23 11:33:42 +0100

    sum first and last array elements

---
+++ JS-Syntax-Fundamentals/Lab/sumFirstAndLastArrayElements.js
@@ -0,0 +1,12 @@
+function solve(input) {
+
+    let firstNum = input[0];
+    let lastNum = input[input.length-1];
+    
+    console.log(firstNum+lastNum);
+
+}
+
+solve([20, 30, 40]);
+solve([10, 17, 22, 33]);
+solve([11, 58, 69]);
