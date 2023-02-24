bc7beb77837229b5a758f9cb44fef4d7602df711
mialskywalker <mialskywalker@gmail.com>
2023-02-24 12:02:57 +0100

    ages

---
+++ JS-Syntax-Fundamentals/Exercies/ages.js
@@ -0,0 +1,31 @@
+function solve(age) {
+    
+    let result;
+
+    if (0 <= age && age <= 2) {
+        result = 'baby';
+    }
+    else if (3 <= age && age <= 13) {
+        result = 'child';
+    }
+    else if (14 <= age && age <= 19) {
+        result = 'teenager';
+    }
+    else if (20 <= age && age <= 65) {
+        result = 'adult';
+    }
+    else if (66 <= age) {
+        result = 'elder';
+    }
+    else {
+        result = 'out of bounds';
+    }
+
+    console.log(result);
+
+}
+
+solve(20);
+solve(1);
+solve(100);
+solve(-1);
