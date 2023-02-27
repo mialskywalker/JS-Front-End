4709204ec31ed4d3c43d168ef9f05d589bed3a5c
mialskywalker <mialskywalker@gmail.com>
2023-02-27 14:00:18 +0100

    reversed chars

---
+++ JS-Syntax-Fundamentals/Exercies/reversedChars.js
@@ -0,0 +1,10 @@
+function solve(first, second, third) {
+
+    let arr = [first, second, third];
+    arr = arr.reverse();
+    console.log(arr.join(' '));
+
+}
+
+solve('A', 'B', 'C');
+solve('1', 'L', '&');
