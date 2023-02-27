60758fa70a35b620496742efb09e34ff281f13db
mialskywalker <mialskywalker@gmail.com>
2023-02-27 13:50:59 +0100

    multiplication table

---
+++ JS-Syntax-Fundamentals/Exercies/multiplicationTable.js
@@ -0,0 +1,16 @@
+function solve(number) {
+
+    let result = 0;
+
+    for (let i = 1; i <= 10; i++) {
+        
+        result = number * i;
+
+        console.log(`${number} X ${i} = ${result}`);
+        
+    }
+
+}
+
+solve(5);
+solve(2);
