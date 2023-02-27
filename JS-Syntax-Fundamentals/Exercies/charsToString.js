90cf5c8186ff12a9bbd431ea2acc29a4594db770
mialskywalker <mialskywalker@gmail.com>
2023-02-27 13:58:30 +0100

    chars to string

---
+++ JS-Syntax-Fundamentals/Exercies/charsToString.js
@@ -0,0 +1,10 @@
+function solve(first, second, third) {
+    
+    let arr = [first, second, third];
+    console.log(arr.join(''));
+
+}
+
+solve('a', 'b', 'c');
+solve('%', '2', 'o');
+solve('1', '5', 'p');
