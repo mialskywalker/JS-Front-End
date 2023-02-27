9e8e15b7ee51d6aea6d12b5e558801117bd745c2
mialskywalker <mialskywalker@gmail.com>
2023-02-27 15:43:41 +0100

    print every nth element from an array

---
+++ JS-Syntax-Fundamentals/Exercies/printEveryNthElementFromAnArray.js
@@ -0,0 +1,16 @@
+function solve(arr, n) {
+
+    let newArr = [];
+
+    for (let i = 0; i < arr.length; i += n) {
+        
+        newArr.push(arr[i]);
+        
+    }
+
+    return newArr;
+}
+
+solve(['5', '20', '31', '4', '20'], 2);
+solve(['dsa', 'asd', 'test', 'tset'], 2);
+solve(['1', '2', '3', '4', '5'], 6);
