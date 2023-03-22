37de41c13a99422b499a787225dd3e907983613c
mialskywalker <mialskywalker@gmail.com>
2023-03-22 14:06:56 +0100

    format grade

---
+++ Functions-And-Statements/Lab/formatGrade.js
@@ -0,0 +1,23 @@
+function FormatGrade(grade) {
+
+    if (grade < 3.00) {
+        console.log(`Fail (2)`);
+    }
+    else if (grade >= 3.00 && grade < 3.50) {
+        console.log(`Poor (${grade.toFixed(2)})`);
+    }
+    else if (grade >= 3.50 && grade < 4.50) {
+        console.log(`Good (${grade.toFixed(2)})`);
+    }
+    else if (grade >= 4.50 && grade < 5.50) {
+        console.log(`Very good (${grade.toFixed(2)})`);
+    }
+    else if (grade >= 5.50) {
+        console.log(`Excellent (${grade.toFixed(2)})`);
+    }
+
+}
+
+FormatGrade(3.33);
+FormatGrade(4.50);
+FormatGrade(2.99);
