5dfa35d03af7a87526d8e7819ba66c8b2aee178f
mialskywalker <mialskywalker@gmail.com>
2023-03-23 13:47:20 +0100

    perfect number

---
+++ Functions-And-Statements/Exercises/perfectNumber.js
@@ -0,0 +1,22 @@
+function PerfectNumber(num) {
+
+    let sum = 0;
+    for (let i = 1; i < num; i++) {
+        if (num % i == 0) {
+            sum += i;
+        }
+    }
+
+    if (sum === num) {
+        console.log('We have a perfect number!');
+    }
+    else {
+        console.log("It's not so perfect.");
+    }
+
+}
+
+
+PerfectNumber(6);
+PerfectNumber(28);
+PerfectNumber(1236498);
