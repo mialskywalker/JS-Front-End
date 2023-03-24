a061cb2d131ab11783cd9ad8fe23c733870061c0
mialskywalker <mialskywalker@gmail.com>
2023-03-24 15:03:23 +0100

    person info

---
+++ Objects-And-Classes/Lab/personInfo.js
@@ -0,0 +1,10 @@
+function PersonInfo(firstName, lastName, age) {
+
+    let person = { firstName, lastName, age };
+
+    return person;
+
+}
+
+console.log(PersonInfo('Peter', 'Pan', 20));
+console.log(PersonInfo('George', 'Smith', 18));
