e236d58e184e8986a182d100eba440eb6b7a0ecd
mialskywalker <mialskywalker@gmail.com>
2023-03-24 15:29:46 +0100

    convert to object

---
+++ Objects-And-Classes/Lab/covertToObject.js
@@ -0,0 +1,12 @@
+function convertToObject(input) {
+
+    let person = JSON.parse(input);
+
+    for (const key in person) {
+        console.log(`${key}: ${person[key]}`);
+    }
+
+}
+
+convertToObject('{"name": "George", "age": 40, "town": "Sofia"}');
+convertToObject('{"name": "Peter", "age": 35, "town": "Plovdiv"}');
