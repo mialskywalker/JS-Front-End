e74ff8de98ebefa70cbd19e334ebbe9dddcb758f
mialskywalker <mialskywalker@gmail.com>
2023-03-31 20:20:55 +0200

    collect list items

--- DOM-and-Events/Lab/03. Collect-List-Items/CollectListItems.js
+++ DOM-and-Events/Lab/03. Collect-List-Items/CollectListItems.js
@@ -1,3 +1,8 @@
 function extractText() {
-    // TODO
+    const liItems = Array.from(document.querySelectorAll('ul > li'));
+    const textArea = document.getElementById('result');
+
+    for (li of liItems) {
+        textArea.textContent += li.textContent + '\n';
+    }
 }
