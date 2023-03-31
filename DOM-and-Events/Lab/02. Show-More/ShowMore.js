4997e3df28014b59879454081fc092d187ec54bb
mialskywalker <mialskywalker@gmail.com>
2023-03-31 20:08:13 +0200

    show more

---
+++ DOM-and-Events/Lab/02. Show-More/ShowMore.js
@@ -0,0 +1,6 @@
+function showText() {
+    const readMore = document.getElementById('more');
+    readMore.style.display = 'none';
+    const text = document.getElementById('text');
+    text.style.display = 'inline';
+}
