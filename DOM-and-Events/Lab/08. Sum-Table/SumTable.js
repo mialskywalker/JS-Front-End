1e3e908f622b7a8eadc2e36720873efd33fea8e4
mialskywalker <mialskywalker@gmail.com>
2023-03-31 22:00:03 +0200

    sum table

--- DOM-and-Events/Lab/08. Sum-Table/SumTable.js
+++ DOM-and-Events/Lab/08. Sum-Table/SumTable.js
@@ -1,3 +1,15 @@
 function sumTable() {
 
+    const tds = Array.from(document.querySelectorAll('td:nth-child(even)'));
+    
+    let sum = 0;
+
+    for (td of tds) {
+        let total = Number(td.textContent);
+        sum += total;
+        if (td.id === 'sum') {
+            td.textContent = sum;
+        }
+    }
+
 }
