e15a68dc9c574d0e8ba5d841c9539fece90e466b
mialskywalker <mialskywalker@gmail.com>
2023-03-31 21:33:13 +0200

    add/delete

--- DOM-and-Events/Lab/06. Add-Delete/app.js
+++ DOM-and-Events/Lab/06. Add-Delete/app.js
@@ -1,3 +1,18 @@
 function addItem() {
-    //TODO...
+    const ulContainer = document.getElementById('items');
+    const inputText = document.getElementById('newItemText');
+    const li = document.createElement("li");
+    const anchor = document.createElement("a");
+    li.textContent = inputText.value;
+    anchor.textContent = '[Delete]';
+    anchor.setAttribute('href', '#');
+    anchor.addEventListener('click', deleteHandler);
+    li.appendChild(anchor);
+    ulContainer.appendChild(li);
+    inputText.value = '';
+
+    function deleteHandler(e) {
+        const eAnchor = e.currentTarget
+        eAnchor.parentElement.remove();
+    }
 }
