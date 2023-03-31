a37f4c8c1113b8f37dd9327a5c360215a042a535
mialskywalker <mialskywalker@gmail.com>
2023-03-31 20:46:35 +0200

    list of items

--- DOM-and-Events/Lab/04. List-Of-Items/app.js
+++ DOM-and-Events/Lab/04. List-Of-Items/app.js
@@ -1,3 +1,10 @@
 function addItem() {
-    console.log('TODO:...');
+    const ulContainer = document.getElementById('items');
+    
+    const inputText = document.getElementById('newItemText');
+    let li = document.createElement("li");
+    let text = inputText.value;
+    li.textContent = text;
+    ulContainer.appendChild(li);
+   
 }
