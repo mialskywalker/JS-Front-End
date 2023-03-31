f942543e6686357fc715439a36166f2b9c02a048
mialskywalker <mialskywalker@gmail.com>
2023-03-31 22:24:31 +0200

    highlight active

--- DOM-and-Events/Lab/09. Highlight-Active/app.js
+++ DOM-and-Events/Lab/09. Highlight-Active/app.js
@@ -1,3 +1,17 @@
 function focused() {
-    console.log('TODO:...');
+    
+    const inputs = Array.from(document.querySelectorAll('div > input'));
+
+    for (el of inputs) {
+        el.addEventListener('focus', (event) => {
+            event.target.parentElement.style.background = "gray";
+            event.target.parentElement.className = "focused";
+            
+        });
+        el.addEventListener("blur", (event) => {
+            event.target.parentElement.style.background = "";
+            event.target.parentElement.classList.remove('focused');
+          });
+    } 
+
 }
