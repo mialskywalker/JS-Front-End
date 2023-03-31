d86e9af7a03493e462f83510d59cff48bd75ceaf
mialskywalker <mialskywalker@gmail.com>
2023-03-31 21:12:30 +0200

    delete from table

--- DOM-and-Events/Lab/05. Delete-From-Table/app.js
+++ DOM-and-Events/Lab/05. Delete-From-Table/app.js
@@ -1,3 +1,21 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+    const mails = Array.from(document.querySelectorAll('td:nth-child(even)'));
+    const inputText = document.querySelector('input[name="email"]');
+    let text = inputText.value;
+    let removed = false;
+    const result = document.getElementById('result');
+    
+    for (mail of mails) {
+        let email = mail.textContent;
+        if (email === text) {
+            mail.parentElement.remove();
+            removed = true;
+        }
+    }
+    if (removed) {
+        result.textContent = 'Deleted.';
+    }
+    else {
+        result.textContent = 'Not found.';
+    }
 }
