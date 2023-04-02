6e7d311f06929697ac195fdc0c0c4e822f7d2da1
mialskywalker <mialskywalker@gmail.com>
2023-04-02 11:12:05 +0200

    accordion

--- DOM-and-Events/Exercises/03. Accordion/accordion.js
+++ DOM-and-Events/Exercises/03. Accordion/accordion.js
@@ -1,3 +1,13 @@
 function toggle() {
-    console.log('TODO:...');
+    const btn = document.querySelector('div > span');
+    const extra = document.getElementById('extra');
+    
+    if (btn.textContent === 'More'){
+        btn.textContent = 'Less';
+        extra.style.display = 'block';
+    }
+    else if (btn.textContent === 'Less') {
+        btn.textContent = 'More';
+        extra.style.display = 'none';
+    }
 }
