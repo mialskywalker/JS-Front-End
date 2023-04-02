c0e5a64ba5ad364e133fbbef83ab53a5277caf2d
mialskywalker <mialskywalker@gmail.com>
2023-04-02 11:54:36 +0200

    locked profile

--- DOM-and-Events/Exercises/04. Locked-Profile/app.js
+++ DOM-and-Events/Exercises/04. Locked-Profile/app.js
@@ -1,3 +1,27 @@
 function lockedProfile() {
-    console.log('TODO...')
+    const buttons = Array.from(document.getElementsByTagName('button'));
+    
+
+    for (btn of buttons) {
+        
+        const profile = btn.parentElement;
+        const hiddenField = profile.children[9];
+        const lock = profile.children[2];
+        const unlock = profile.children[4];
+
+        btn.addEventListener('click', (e) => {
+            const button = e.currentTarget;
+            if (unlock.checked) {
+                if (button.textContent === 'Show more'){
+                    hiddenField.style.display = 'block';
+                    button.textContent = 'Hide it';
+                }
+                else if (button.textContent === 'Hide it'){
+                    hiddenField.style.display = 'none';
+                    button.textContent = 'Show more';
+                }
+            }
+        });
+    }
+
 }
