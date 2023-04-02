3435bca81a7e3111b623974a749830d19821c690
mialskywalker <mialskywalker@gmail.com>
2023-04-02 13:03:02 +0200

    format the text

--- DOM-and-Events/Exercises/07. Format the Text/solution.js
+++ DOM-and-Events/Exercises/07. Format the Text/solution.js
@@ -1,3 +1,17 @@
 function solve() {
-  //TODO
+
+  const output = document.getElementById('output');
+  const inputText = document.getElementById('input').value;
+
+  const text = inputText.split('.');
+  text.pop();
+  
+  while (text.length > 0) {
+    
+    const newParagraph = document.createElement('p');
+    newParagraph.textContent = text.splice(0, 3).join('.') + '.';
+    output.appendChild(newParagraph);
+
+  }
+
 }
