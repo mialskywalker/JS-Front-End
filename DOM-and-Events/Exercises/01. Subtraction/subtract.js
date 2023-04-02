685a2db069e52735d2dde775b952bbc9d610ba6d
mialskywalker <mialskywalker@gmail.com>
2023-04-02 10:50:47 +0200

    subtraction

---
+++ DOM-and-Events/Exercises/01. Subtraction/subtract.js
@@ -0,0 +1,8 @@
+function subtract() {
+    const firstInput = document.getElementById('firstNumber');
+    const secondInput = document.getElementById('secondNumber');
+    const result = document.getElementById('result');
+
+    result.textContent = Number(firstInput.value) - Number(secondInput.value);
+
+}
