fb5be043433cfb52793f2725369455cb9b2b01fb
mialskywalker <mialskywalker@gmail.com>
2023-04-02 12:44:16 +0200

    table - search engine

--- DOM-and-Events/Exercises/06. Table - Search Engine/solution.js
+++ DOM-and-Events/Exercises/06. Table - Search Engine/solution.js
@@ -2,7 +2,22 @@ function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
 
    function onClick() {
-      //   TODO:
+      
+      const textInput = document.getElementById('searchField');
+      const tds = Array.from(document.querySelectorAll('tbody > tr > td'));
+      const trs = Array.from(document.querySelectorAll('tbody > tr'));
+      
+      for (tr of trs) {
+         tr.classList.remove('select');
+      }
+
+      for (td of tds) {
+         if (td.textContent.includes(textInput.value)) {
+            const newTr = td.parentElement;
+            newTr.classList.add('select');
+         }
+      }
+      textInput.value = '';
 
    }
 }
