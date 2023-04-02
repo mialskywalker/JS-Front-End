5258d839956826149d027439833f93593c3bdd87
mialskywalker <mialskywalker@gmail.com>
2023-04-02 12:04:30 +0200

    fill dropdown

--- DOM-and-Events/Exercises/05. Fill-Dropdown/dropdown.js
+++ DOM-and-Events/Exercises/05. Fill-Dropdown/dropdown.js
@@ -1,3 +1,15 @@
 function addItem() {
-    console.log('TODO:...');
+    const menu = document.getElementById('menu');
+    const textInput = document.getElementById('newItemText');
+    const valueInput = document.getElementById('newItemValue');
+
+    const option = document.createElement('option');
+
+    option.textContent = textInput.value;
+    option.value = valueInput.value;
+    menu.appendChild(option);
+
+    textInput.value = '';
+    valueInput.value = '';
+
 }
